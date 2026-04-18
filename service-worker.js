acc092758e3fbb280cf02b9e4213bf7d184dc4c5
fafa <doudoumetis@yahoo.fr>
2026-04-18 20:59:52 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'fafatraining-v33';
-const ASSETS = ['.','index.html','style.css','app.js','manifest.json','assets/logo.jpeg','data/exercises.json','icons/icon-192.png','icons/icon-512.png'];
-self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request))));
