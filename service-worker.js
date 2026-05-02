2468cf90567ce44d3a8cdc08ec288c53f3040b40
fafa <doudoumetis@yahoo.fr>
2026-05-02 15:48:20 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='fafatraining-v37-ultra-intelligente';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./data/exercises.json','./data/programs.json','./data/visual_map.json','./data/level_rules.json','./data/progression.json','./data/injury_groups.json','./data/equipment_groups.json','./data/scales.json','./data/quick_goals.json','./data/places.json','./data/ultra_rules.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
